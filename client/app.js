1ed252899457c35f57ddb926f0b3a9c569f29ca6
Urvi Gupta <121297598+urviiigupta@users.noreply.github.com>
2024-08-15 22:42:43 +0530

    init

--- client/app.js
+++ client/app.js
@@ -2,12 +2,10 @@ const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('node:path');
 const { v4: uuidv4 } = require('uuid');
 const screenshot = require('screenshot-desktop');
-const robot = require('robotjs');
-
 
 var socket;
 try {
-  socket = require('socket.io-client')('http://localhost:5000');
+    socket = require('socket.io-client')('http://localhost:5000');
     console.log('Socket connected');
 } catch (error) {
     console.error('Failed to connect to server:', error);
@@ -20,8 +18,7 @@ function createWindow() {
         width: 500,
         height: 150,
         webPreferences: {
-           // preload: path.join(__dirname, 'preload.js')
-           nodeIntegration: true, // Enable Node.js integration
+            nodeIntegration: true, // Enable Node.js integration
             contextIsolation: false // Disable context isolation
         }
     });
@@ -29,26 +26,45 @@ function createWindow() {
     win.loadFile('index.html');
     win.webContents.openDevTools();
 
+    // Handle mouse movements
     socket.on('mouse-move', (data) => {
-        var obj=JSON.parse(data);
-        var x=obj.x;
-        var y=obj.y;
-        robot.moveMouse(x, y);
-
-
+        var obj = JSON.parse(data);
+        var x = obj.x;
+        var y = obj.y;
+        win.webContents.sendInputEvent({
+            type: 'mousemove',
+            x: x,
+            y: y
+        });
     });
 
+    // Handle mouse clicks
     socket.on('mouse-click', (data) => {
-        robot.mouseClick();
-
+        win.webContents.sendInputEvent({
+            type: 'mouseDown',
+            button: 'left',
+            clickCount: 1
+        });
+        win.webContents.sendInputEvent({
+            type: 'mouseUp',
+            button: 'left',
+            clickCount: 1
+        });
     });
 
+    // Handle key presses
     socket.on('type', (data) => {
-        var obj=JSON.parse(data);
-        var key=obj.key;
-        robot.keyTap(key);
+        var obj = JSON.parse(data);
+        var key = obj.key;
+        win.webContents.sendInputEvent({
+            type: 'keyDown',
+            keyCode: key
+        });
+        win.webContents.sendInputEvent({
+            type: 'keyUp',
+            keyCode: key
+        });
     });
-
 }
 
 app.whenReady().then(() => {
@@ -68,24 +84,23 @@ app.on('window-all-closed', () => {
 });
 
 ipcMain.on('start-share', function (event, arg) {
-  console.log('start-share event received');
-  var uuid = "test";
-  console.log('Generated UUID:', uuid);
-  socket.emit('join-message', uuid);
-  event.reply('uuid', uuid);
+    console.log('start-share event received');
+    var uuid = "test";
+    console.log('Generated UUID:', uuid);
+    socket.emit('join-message', uuid);
+    event.reply('uuid', uuid);
 
-  interval = setInterval(() => {
-    screenshot().then((img) => {
-      var imgStr=new Buffer(img).toString('base64');
-      var obj={};
-      obj.room=uuid;
-      obj.image=imgStr;
+    interval = setInterval(() => {
+        screenshot().then((img) => {
+            var imgStr = new Buffer(img).toString('base64');
+            var obj = {};
+            obj.room = uuid;
+            obj.image = imgStr;
 
-      socket.emit('screen-data',JSON.stringify(obj));
-    });
-  }, 100);
+            socket.emit('screen-data', JSON.stringify(obj));
+        });
+    }, 100);
 });
-
 
 ipcMain.on('stop-share', function (event, arg) {
     console.log('stop-share event received');
