ba8c98f301184e226d03f21ab75636acc264d8c9
Urvi Gupta <121297598+urviiigupta@users.noreply.github.com>
2024-08-15 21:30:43 +0530

    init

---
+++ client/app.js
@@ -0,0 +1,70 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('node:path');
+const { v4: uuidv4 } = require('uuid');
+const screenshot = require('screenshot-desktop');
+
+var socket;
+try {
+  socket = require('socket.io-client')('http://localhost:5000');
+    console.log('Socket connected');
+} catch (error) {
+    console.error('Failed to connect to server:', error);
+}
+
+var interval;
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 500,
+        height: 150,
+        webPreferences: {
+           // preload: path.join(__dirname, 'preload.js')
+           nodeIntegration: true, // Enable Node.js integration
+            contextIsolation: false // Disable context isolation
+        }
+    });
+    win.removeMenu();
+    win.loadFile('index.html');
+    win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+ipcMain.on('start-share', function (event, arg) {
+  console.log('start-share event received');
+  var uuid = uuidv4();
+  console.log('Generated UUID:', uuid);
+  socket.emit('join-message', uuid);
+  event.reply('uuid', uuid);
+
+  interval = setInterval(() => {
+    screenshot().then((img) => {
+      var imgStr=new Buffer(img).toString('base64');
+      var obj={};
+      obj.room=uuid;
+      obj.image=imgStr;
+
+      socket.emit('screen-data',JSON.stringify(obj));
+    });
+  }, 100);
+});
+
+
+ipcMain.on('stop-share', function (event, arg) {
+    console.log('stop-share event received');
+    clearInterval(interval);
+});
---
+++ server/index.js
@@ -0,0 +1,32 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var cors = require('cors');
+
+// Use CORS middleware
+app.use(cors());
+
+app.get('/view', (req, res) => {
+    res.sendFile(__dirname + '/display.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('join-message', (roomId) => {
+        socket.join(roomId);
+        console.log("User joined room: " + roomId);
+    });
+
+    socket.on('screen-data', (data) => {
+        data = JSON.parse(data);
+        var room = data.room;
+        var imgStr = data.image;
+        socket.broadcast.to(room).emit('screen-data', imgStr);
+    });
+});
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
+
+http.listen(server_port, () => {
+    console.log("Server started on port " + server_port);
+});
+
